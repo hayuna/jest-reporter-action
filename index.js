da0dac1165bf7ef34aa8ee1a28d41a64390dc3f6
hayuna <marek.kowalonek@gmail.com>
2020-09-12 11:16:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const { execSync } = require("child_process");
 const { GitHub, context } = require("@actions/github");
 
 const main = async () => {
-  const repoName = context.repo.repo;
-  const repoOwner = context.repo.owner;
-  const githubToken = core.getInput("github-token");
-  const testCommand = core.getInput("test-command") || "npx jest";
-  const prNumber = context.payload.number;
+  const githubClient = new GitHub(core.getInput("github-token"));
 
-  const githubClient = new GitHub(githubToken);
-
-  const codeCoverage = execSync(testCommand).toString();
+  const codeCoverage = execSync(core.getInput("test-command") || "npx jest").toString();
+  const regex = /.*(File.* \|)/gs;
+  const content = regex.exec(codeCoverage)[1];
   let coveragePercentage = execSync(
     "npx coverage-percentage ./coverage/lcov.info --lcov"
   ).toString();
@@ -20,15 +16,15 @@ const main = async () => {
   const commentBody = `<p>Total Coverage: <code>${coveragePercentage}</code></p>
 <details><summary>Coverage report</summary>
 <p>
-<pre>${codeCoverage}</pre>
+${content}
 </p>
 </details>`;
 
   await githubClient.issues.createComment({
-    repo: repoName,
-    owner: repoOwner,
+    repo: context.repo.repo,
+    owner: context.repo.owner,
     body: commentBody,
-    issue_number: prNumber,
+    issue_number: context.payload.number,
   });
 };
 
